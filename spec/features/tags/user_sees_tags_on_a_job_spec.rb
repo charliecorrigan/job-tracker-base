2ad643b52bd3955166514625513f52dc2f6781ec
ccorrigan <charlie.ps.corrigan@gmail.com>
2017-06-06 10:26:22 -0600

    Create jobs_tags migration

--- spec/features/tags/user_sees_tags_on_a_job_spec.rb
+++ spec/features/tags/user_sees_tags_on_a_job_spec.rb
@@ -4,8 +4,8 @@ describe "User sees tags associated with a job" do
   scenario "a user sees all tags for a specific job" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    tag1 = Tag.create("Software")
-    tag2 = Tag.create("Good-Location")
+    tag1 = Tag.create(name: "Software")
+    tag2 = Tag.create(name: "Good-Location")
     jobs_tags1 = JobsTag.create(job_id: job.id, tag_id: tag1.id)
     jobs_tags2 = JobsTag.create(job_id: job.id, tag_id: tag2.id)
 
